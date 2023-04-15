defed34efb4e14f9ae26e52d604b6b4df35f595f
Patrykowski8 <130091590+Patrykowski8@users.noreply.github.com>
2023-04-15 22:54:07 +0200

    Zapisywanie wyniku w plikach gry

--- Assets/Points.cs
+++ Assets/Points.cs
@@ -8,6 +8,17 @@ public class Points : MonoBehaviour
     public TextMeshProUGUI scoretext;
     [SerializeField]
     private int NumberStart, NumberAdding;
+    private void Start()
+    {
+        NumberStart = PlayerPrefs.GetInt("score");
+        scoretext.text = NumberStart.ToString();
+    }
+
+    private void Update()
+    {
+        PlayerPrefs.SetInt("score", int.Parse(scoretext.text));
+    }
+
     public void Score()
     {
        
