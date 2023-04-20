4147440984249b91c3b48d5aeef6fa3725ed7b4b
Marta <tulciak189@gmail.com>
2023-04-20 20:17:23 +0200

    Update2SoundAndSettings

---
+++ Assets/volumeScene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Audio;
+
+public class volumeScene : MonoBehaviour
+{
+    public AudioSource VolumeMusic;
+
+    void Start()
+    {
+        VolumeMusic.volume = PlayerPrefs.GetFloat("MusicVolume");
+    }
+
+    void Update()
+    {
+        
+    }
+}
