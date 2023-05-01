5c3f47a5d2665ff04dc64f2f0390f3705efe9dd8
Marta <tulciak189@gmail.com>
2023-05-01 19:34:29 +0200

    UpgradePanel

--- Assets/Script/LevelAdding.cs
+++ Assets/Script/LevelAdding.cs
@@ -5,27 +5,112 @@ using TMPro;
 using UnityEngine.UI;
 public class LevelAdding : MonoBehaviour
 {
-    public TextMeshProUGUI LevelAdd;
+    public TextMeshProUGUI StatusWyboru;
+    public Image SquareOne;
+    public Image SquareTwo;
+    public Image SquareThree;
+    public Image SquareFour;
+    public Image SquareFive;
     [SerializeField]
+    private int LevelAdd;
+    public int SquareIntOne;
+    public int SquareIntTwo;
+    public int SquareIntThree;
+    public int SquareIntFour;
+    public int SquareIntFive;
     private int NumberStart;
+    /*
+     * Skills Id=0
+     * Room Id=1
+     * Computer Id=2
+     * Desk Id=3
+     * Chair Id=4
+     */
+    
     private void Start()
     {
-
-        LevelAdd.text = (PlayerPrefs.GetInt("leveladd")).ToString();
-        if (LevelAdd.text == 0.ToString())
+    SquareIntOne = 0;
+    SquareIntTwo = 0;
+    SquareIntThree = 0;
+    SquareIntFour = 0;
+    SquareIntFive = 0;
+        /*
+        if (PlayerPrefs.GetInt("SquareIntOne") >= 1)
         {
-            LevelAdd.text = 1.ToString();
+            SquareIntOne = PlayerPrefs.GetInt("SquareIntOne");
         }
-        NumberStart = PlayerPrefs.GetInt("level");
-       
+        if (PlayerPrefs.GetInt("SquareIntTwo") >= 1)
+        {
+            SquareIntTwo = PlayerPrefs.GetInt("SquareIntTwo");
+        }
+        if (PlayerPrefs.GetInt("SquareIntThree") >= 1)
+        {
+            SquareIntThree = PlayerPrefs.GetInt("SquareIntThree");
+        }
+        if (PlayerPrefs.GetInt("SquareIntFour") >= 1)
+        {
+            SquareIntFour = PlayerPrefs.GetInt("SquareIntFour");
+        }
+        if (PlayerPrefs.GetInt("SquareIntFive") >= 1)
+        {
+            SquareIntFive = PlayerPrefs.GetInt("SquareIntFive");
+        }
+        */
     }
-    
+    private void Update()
+    {
+        if(int.Parse(StatusWyboru.text) == 0)
+        {
+            if (SquareIntOne >= 5)
+            {
+                SquareOne.color = Color.green;
+            }
+            if (SquareIntOne >= 10)
+            {
+                SquareTwo.color = Color.green;
+            }
+            if (SquareIntOne >= 15)
+            {
+                SquareThree.color = Color.green;
+            }
+            if (SquareIntOne >= 20)
+            {
+                SquareFour.color = Color.green;
+            }
+            if (SquareIntOne >= 25)
+            {
+                SquareFive.color = Color.green;
+            }
+        }
+        
+    }
     public void AddLevel()
     {
-        NumberStart = NumberStart + int.Parse(LevelAdd.text);
-  
+        switch (int.Parse(StatusWyboru.text))
+        {
+            case 0:
+                SquareIntOne += 1;
+                PlayerPrefs.SetInt("SquareIntOne", SquareIntOne);
+                Debug.Log(SquareIntOne);
 
+
+
+                break;
+            case 1:
+                
+                break;
+        }
     }
+    public void ClearSquare()
+    {
     
+        SquareOne = SquareOne.GetComponent<Image>();
+        SquareTwo = SquareTwo.GetComponent<Image>();
+        SquareThree = SquareThree.GetComponent<Image>();
+        SquareFour = SquareFour.GetComponent<Image>();
+        SquareFive = SquareFive.GetComponent<Image>();
+    }
+
    
+
 }
